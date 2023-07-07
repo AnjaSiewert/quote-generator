abe0e7475ed28a7a58686a41479da84a5f000ee1
Anja Siewert <anja.siewert@live.de>
2023-07-07 14:20:44 +0200

    add functions to buttons

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterButton = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show New Quote
 
 function newQuote() {
-    // Pick a random qupte from apiQuotes array
+    // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    // Check if author field is blank and replace it with 'Unknown'
+    if(!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check the code length to determine styling
+    if(quote.text.length > 100) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    };
+    quoteText.textContent = quote.text;
 };
 
 // Get quotes from API
@@ -20,5 +38,16 @@ async function getQuotes() {
         alert(error);
     }
 };
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterURL, '_blank');
+};
+
+// Event Listeners
+newQuoteButton.addEventListener('click', newQuote);
+twitterButton.addEventListener('click', tweetQuote);
+
 // On Load
 getQuotes();
